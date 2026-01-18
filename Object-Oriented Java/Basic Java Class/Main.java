5fb94c9ba4b76691abc02165dc8e0845ff379c65
Azman Sikder <azmansikder143@gmail.com>
2026-01-19 01:43:40 +0600

    Refactor Store object instantiation and output

--- Object-Oriented Java/Basic Java Class/Main.java
+++ Object-Oriented Java/Basic Java Class/Main.java
@@ -1,10 +1,10 @@
 public class Main{
   public static void main(String[] args) {
-    Store lemonadeStand = new Store("lemonade", 42, .99);
-    Store cookieShop = new Store("cookies", 12, 3.75);
+    Store Obj1 = new Store("lemonade", 42, .99);
+    Store Obj2 = new Store("cookies", 12, 3.75);
     
-    System.out.println("Our first shop sells " + lemonadeStand.productType + " at " + lemonadeStand.inventoryPrice + " per unit.");
+    System.out.println("Our first shop sells " + Obj1.productType + " at " + Obj1.inventoryPrice + " per unit.");
     
-    System.out.println("Our second shop has " + cookieShop.inventoryCount + " units remaining.");
+    System.out.println("Our second shop has " + Obj2.inventoryCount + " units remaining.");
   }
 }
