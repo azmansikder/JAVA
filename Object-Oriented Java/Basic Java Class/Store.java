1c11403947fc153f2c7f6dcd256b028605248850
Azman Sikder <azmansikder143@gmail.com>
2026-01-19 01:33:12 +0600

    Create Store class with fields and constructor

---
+++ Object-Oriented Java/Basic Java Class/Store.java
@@ -0,0 +1,13 @@
+public class Store {
+  // instance fields
+  String productType;
+  int inventoryCount;
+  double inventoryPrice;
+  
+  // constructor method
+  public Store(String product, int count, double price) {
+    productType = product;
+    inventoryCount = count;
+    inventoryPrice = price;
+  }
+}
