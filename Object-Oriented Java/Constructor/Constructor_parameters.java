202ee299435264f2ab6f9f74640ff0f034838ba7
Azman Sikder <azmansikder143@gmail.com>
2026-01-19 03:12:34 +0600

    Add Car class with default attributes

---
+++ Object-Oriented Java/Constructor/Constructor_parameters.java
@@ -0,0 +1,10 @@
+public class Car { 
+ public String color = "red"; 
+ public boolean isElectric = false; 
+ public int cupHolders = 4; 
+
+ public static void main(String[] args) { 
+   Car myCar = new Car(); 
+   System.out.println(myCar.color); // Prints: red 
+ } 
+} 
